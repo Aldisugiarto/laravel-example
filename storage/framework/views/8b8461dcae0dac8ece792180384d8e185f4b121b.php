ed99a5af7f0a70103baf7c126971c71f6f69ba09
Aldi Sugiarto:@Lakuemas22 <aldisugiarto@lakuemas.com>
2022-06-16 15:11:07 +0700

    Delete files

--- storage/framework/views/8b8461dcae0dac8ece792180384d8e185f4b121b.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('title', __('Not Found')); ?>
-<?php $__env->startSection('code', '404'); ?>
-<?php $__env->startSection('message', __('Not Found')); ?>
-
-<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\aldis\OneDrive\Documents\2. Project\2. Source Code\Learn\laravel\coba-laravel\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
